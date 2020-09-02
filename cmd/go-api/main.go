f628a8d5f4749f2a1fb1ca06b3e9455b31bfcf66
christian-bull <cbull.projects@gmail.com>
2020-09-02 16:08:44 -0400

    moved query to operate when endpoint is hit

--- cmd/go-api/main.go
+++ cmd/go-api/main.go
@@ -29,6 +29,7 @@ type Tweet struct {
 
 // Statuses list of status
 var status Status
+var db *sql.DB
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Base page")
@@ -39,6 +40,16 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func trailStatus(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: trail status")
 	w.Header().Set("Content-Type", "application/json")
+
+	t := Tweet{}
+
+	err := db.QueryRow("SELECT ID, MAX(created_at), tweet_text, status FROM tweets", 1).Scan(
+		&t.ID, &t.createdat, &t.text, &t.status,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	status := Status{ID: t.ID, Status: t.status, Text: t.text}
 	json.NewEncoder(w).Encode(status)
 }
 
@@ -50,26 +61,11 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":2000", myRouter))
 }
 
-func getStatus(db *sql.DB) Status {
-	t := Tweet{}
-
-	err := db.QueryRow("SELECT ID, MAX(created_at), tweet_text, status FROM tweets", 1).Scan(
-		&t.ID, &t.createdat, &t.text, &t.status,
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return Status{ID: t.ID, Status: t.status, Text: t.text}
-}
-
 func main() {
 	// open db and defer close until execute
-	sqliteDatabase, err := sql.Open("sqlite3", os.Getenv("sqldatabase"))
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer sqliteDatabase.Close()
-	status = getStatus(sqliteDatabase)
+	db, _ = sql.Open("sqlite3", os.Getenv("sqldatabase"))
+
+	defer db.Close()
 
 	// run server
 	handleRequests()
